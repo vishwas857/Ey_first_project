9c229dc16e4fcfa2beac50283fa5ec4a1e64a997
Vishwas <159612031+vishwas857@users.noreply.github.com>
2025-11-20 19:08:27 +0530

    third commit

--- app.py
+++ app.py
@@ -218,5 +218,9 @@ def download_essay():
     )
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port)
+
